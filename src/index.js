a3cccfba4fb2129ae6551339ef5a87e5779b1ca3
DmitriiTMS <dmitrii12_88@mail.ru>
2024-08-06 22:01:58 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+
+    let resArray = [];
+
+    matrix.forEach((elem, index, matrixArr) => {
+        if (index % 2 === 0) {
+            resArray.push(...matrixArr[index]);
+        } else {
+            resArray.push(...matrixArr[index].reverse());
+        }
+    });
+
+    return resArray;
+};
